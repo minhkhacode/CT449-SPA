82fd2268fc00dd0e08e3a86e54310c038f452761
Kha <nkha2888@gmail.com>
2023-09-21 21:36:54 +0700

    Cai dat xu ly loi api-error

--- app.js
+++ app.js
@@ -4,11 +4,25 @@ const cors = require('cors');
 const app = express();
 
 const contactsRouter = require('./app/routes/contactRoute');
+const ApiError = require('./app/api-error');
 
 app.use(cors());
 app.use(express.json());
 
 app.use('/api/contacts/', contactsRouter);
+
+app.use((req, res, next) => {
+    // Khi khong co endpoint duoc goi thi vao day
+    return next(new ApiError(404, "Resource not found"));
+})
+
+// Error handling
+app.use((err, req, res, next) => {
+
+    return res.status(err.statusCode || 500).json({
+        message: err.message || "Internal Server Error",
+    })
+})
 
 app.get("/", (req, res) => {
     res.status(200).json({
