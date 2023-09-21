cd01f351c7748986d75ccfa02c7aeb1ba0527096
Kha <nkha2888@gmail.com>
2023-09-21 23:24:33 +0700

    update app error handling

--- app.js
+++ app.js
@@ -11,6 +11,12 @@ app.use(express.json());
 
 app.use('/api/contacts/', contactsRouter);
 
+app.get("/", (req, res) => {
+    res.status(200).json({
+        message: "Welcome to contact book application.",
+    })
+})
+
 app.use((req, res, next) => {
     // Khi khong co endpoint duoc goi thi vao day
     return next(new ApiError(404, "Resource not found"));
@@ -21,12 +27,6 @@ app.use((err, req, res, next) => {
 
     return res.status(err.statusCode || 500).json({
         message: err.message || "Internal Server Error",
-    })
-})
-
-app.get("/", (req, res) => {
-    res.status(200).json({
-        message: "Welcome to contact book application.",
     })
 })
 
